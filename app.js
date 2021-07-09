e50ca12f0bb895a70fb5156a0ed3eb791ba601c3
iEmbrion <garyang92@hotmail.com>
2021-07-09 19:55:07 +0800

    Initial commit

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express'); //Framework to simplify coding
 const morgan = require('morgan'); //Use for logging
 
+const AppError = require('./utils/appError');
+const globalErrHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -16,10 +18,6 @@ app.use(express.json());
 
 //Our own middle ware
 //Note that 3rd party middlewares like express.json() returns the very same callback used below (req,res,next => ....)
-app.use((req, res, next) => {
-  // console.log('Hello from the middleware!! 😊😊😊');
-  next();
-});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -30,5 +28,18 @@ app.use((req, res, next) => {
 //Mounting routers on a route
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+
+  //if a param is passed into next(), express will treat it as an error object and automactically skip all the other middlewares and send it to the global error handling middleware
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+//4) Error handling middleware
+app.use(globalErrHandler);
 
 module.exports = app;
