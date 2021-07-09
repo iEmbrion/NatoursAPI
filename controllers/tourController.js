f1d6a8a6e9edd065500573856dee423788ab0d71
iEmbrion <garyang92@hotmail.com>
2021-07-09 17:56:39 +0800

    Finished Section 8

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -21,7 +21,7 @@ exports.getAllTours = async (req, res) => {
 
     res.status(200).json({
       status: 'success',
-      // results: tours.length,
+      results: tours.length,
       data: {
         tours,
       },
@@ -66,7 +66,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      message: `Invalid data sent!`,
+      message: err.message,
     });
   }
 };
--- models/tourModel.js
+++ models/tourModel.js
@@ -1,57 +1,128 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
+// const validator = require('validator');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    trim: true,
-    unique: true,
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      trim: true,
+      unique: true,
+      // validate: [validator.isAlpha, 'Tour name must only contain characters'],
+    },
+    slug: String,
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour should have a difficulty'],
+      enum: ['easy', 'medium', 'difficult'],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 3.0,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          // .this only points to cur doc on NEW doc creation
+          return val < this.price;
+        },
+        message: `Discount price ({VALUE}) should be below regular price`,
+      },
+    },
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+      select: false,
+    },
   },
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a duration'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour should have a difficulty'],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 3.0,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'A tour must have a description'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image'],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
-  startDates: [Date],
+  {
+    //Include virtual when data gets outputted in JSON or object
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
+});
+
+//Mongoose Middlewares
+//Note: the hook (E.g. save, find) will determine the type of middleware (E.g. 'save' will get a document middleware)
+
+//Document Middleware: runs before .save() and .create()
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.pre('save', function (next) {
+  // console.log('Will save document...');
+  next();
+});
+
+tourSchema.post('save', function (doc, next) {
+  // console.log(doc);
+  next();
+});
+
+//Query Middleware
+
+//Note: The find hook doesn't work for findOne, so use regex
+tourSchema.pre(/^find/, function (next) {
+  this.find({ secretTour: { $ne: true } });
+  this.startTime = Date.now();
+  next();
+});
+
+tourSchema.post(/^find/, function (docs, next) {
+  console.log(`Time taken: ${Date.now() - this.startTime} ms`);
+  next();
+});
+
+//Aggregation Middleware
+tourSchema.pre('aggregate', function (next) {
+  console.log(
+    this.pipeline().unshift({ $match: { secretTour: { $ne: true } } })
+  );
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
